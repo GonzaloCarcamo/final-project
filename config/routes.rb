319e3885a152fd5a753510445fba328a843146fa
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2020-02-22 17:46:56 -0300

    creacion rutas, root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :tags
   resources :categories
   resources :ads
+
+  root 'ads#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
